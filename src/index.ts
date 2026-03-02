41a7a5a6f7138fe34f9bdd206fbc0b7b86be41b1
Andrew Norman <Anormy123@gmail.com>
2026-03-01 21:28:10 -0600

    Added an event.

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@ import { writeFileSync, rmSync, mkdirSync } from 'node:fs'
 
 const events: EventAttributes[] = [
   {
+    productId: 'mgtf',
     start: [2026, 5, 1],
-    duration: {
-      hours: 2
-    },
-    title: 'Test event',
-    location: 'Somewhere',
-    url: 'https://maplegrovetrack.github.io/'
+    end: [2026, 5, 2],
+    startInputType: 'local',
+    startOutputType: 'local',
+    endInputType: 'local',
+    endOutputType: 'local',
+    title: 'OMNI Community Night',
+    location: 'OMNI Brewing Company',
+    status: 'CONFIRMED',
+    url: 'https://maplegrovetrack.github.io/events#monday-march-2-2026'
   }
 ]
 
